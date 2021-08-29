8b8410fddfd73a90d8fba7f1dd270eaa76ad77a9
Le, Vu Hoan Thieu <levu74@gmail.com>
2021-08-29 17:29:12 +0700

    Add Morse code decoder

---
+++ CodingKata.Exercise.Tests/CodeWars/MorseCodeDecoderTests.cs
@@ -0,0 +1,30 @@
+﻿using CodingKata.Exercise.CodeWars.MorseCodeDecoder;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodingKata.Exercise.Tests.CodeWars
+{
+    public class MorseCodeDecoderTests
+    {
+        private Kata _sut;
+
+        public MorseCodeDecoderTests()
+        {
+            _sut = new Kata();
+        }
+        [Theory]
+       
+        [InlineData(".... . -.--   .--- ..- -.. .", "HEY JUDE")]
+        public void DecodeTest(string morseCode, string expected)
+        {
+            var decode = _sut.Decode(morseCode);
+
+            decode.Should().Be(expected);
+        }
+    }
+}
---
+++ CodingKata.Exercise/CodeWars/MorseCodeDecoder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CodingKata.Exercise.CodeWars.MorseCodeDecoder
+{
+
+    public class Kata
+    {
+        public string Decode(string morseCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
