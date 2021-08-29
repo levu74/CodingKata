226d1d895e77f7cdf2e352f79bbeb58666e9ef98
Le, Vu Hoan Thieu <levu74@gmail.com>
2021-08-29 17:11:50 +0700

    Submit BitCounting

---
+++ CodingKata.Exercise.Tests/CodeWars/BitCountingTests.cs
@@ -0,0 +1,32 @@
+﻿using CodingKata.Exercise.CodeWars.BitCounting;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodingKata.Exercise.Tests.CodeWars
+{
+    
+    public class BitCountingTests
+    {
+        private Kata _sut;
+
+        public BitCountingTests()
+        {
+            _sut = new Kata();
+        }
+        [Theory]
+        [InlineData(10, 2)]
+        [InlineData(9, 2)]
+        [InlineData(7, 3)]
+        [InlineData(77231418, 14)]
+        public void CountBitsTest(int decimalNumber, int bitCount)
+        {
+            var bitCountResult = _sut.CountBits(decimalNumber);
+            bitCountResult.Should().Be(bitCount);
+        }
+    }
+}
---
+++ CodingKata.Exercise/CodeWars/BitCounting.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CodingKata.Exercise.CodeWars.BitCounting
+{
+
+    public class Kata
+    {
+        public int CountBits(int n)
+        {
+            int count = 0;
+            int remain = n;
+            int maxBits = 0;
+
+            while (Math.Pow(2, maxBits) <= n)
+            {
+                maxBits = maxBits + 1;
+            }
+
+            for (int i = maxBits; i >= 0; i--)
+            {
+                var exponent = (int)Math.Pow(2, i);
+                if (exponent == remain)
+                {
+                    count++;
+                    break;
+                } 
+                else if (exponent < remain)
+                {
+                    count++;
+                    remain = remain - exponent;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            return count;
+        }
+    }
+}
