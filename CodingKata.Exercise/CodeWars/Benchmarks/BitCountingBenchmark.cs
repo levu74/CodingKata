18ed75c63cf1e446310131a0f2fc4c6230ac33f4
Le, Vu Hoan Thieu <levu74@gmail.com>
2021-08-30 09:24:49 +0700

    Add benchmark for katas

---
+++ CodingKata.Exercise/CodeWars/Benchmarks/BitCountingBenchmark.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace CodingKata.Exercise.CodeWars.Benchmarks
+{
+    public class BitCountingBenchmark
+    {
+        private const int Number = 500;
+
+        [Benchmark]
+        public void MySolution()
+        {
+            new BitCounting.Kata().CountBits(Number);
+        }
+
+
+        [Benchmark]
+        public void BestVote()
+        {
+            new BitCounting.KataBestVote().CountBits(Number);
+        }
+    }
+}
