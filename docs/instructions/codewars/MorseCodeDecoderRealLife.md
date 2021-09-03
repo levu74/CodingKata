527e929083aefdab2472afa06bdba146e4a74cb9
Le, Vu Hoan Thieu <levu74@gmail.com>
2021-09-03 18:06:58 +0700

    Morse code decoder in real life

---
+++ docs/instructions/codewars/MorseCodeDecoderRealLife.md
@@ -0,0 +1,60 @@
+Part of Series 3/3
+
+This kata is part of a series on the Morse code. Make sure you solve the \[first part\](/kata/decode-the-morse-code) and the \[second part\](/kata/decode-the-morse-code-advanced) and then reuse and advance your code to solve this one.
+
+  
+In this kata you have to deal with "real-life" scenarios, when Morse code transmission speed slightly varies throughout the message as it is sent by a non-perfect human operator. Also the sampling frequency may not be a multiple of the length of a typical "dot".
+
+For example, the message `HEY JUDE`, that is `···· · −·−−   ·−−− ··− −·· ·` may actually be received as follows:
+
+`0000000011011010011100000110000001111110100111110011111100000000000111011111111011111011111000000101100011111100000111110011101100000100000`
+
+As you may see, this transmission is generally accurate according to the standard, but some dots and dashes and pauses are a bit shorter or a bit longer than the others.
+
+Note also, that, in contrast to the previous kata, the estimated average rate (bits per dot) may not be a whole number – as the hypotetical transmitter is a human and doesn't know anything about the receiving side sampling rate.
+
+For example, you may sample line 10 times per second (100ms per sample), while the operator transmits so that his dots and short pauses are 110-170ms long. Clearly 10 samples per second is enough resolution for this speed (meaning, each dot and pause is reflected in the output, nothing is missed), and dots would be reflected as 1 or 11, but if you try to estimate rate (bits per dot), it would not be 1 or 2, it would be about (110 + 170) / 2 / 100 = 1.4. Your algorithm should deal with situations like this well.
+
+Also, remember that each separate message is supposed to be possibly sent by a different operator, so its rate and other characteristics would be different. So you have to analyze each message (i. e. test) independently, without relying on previous messages. On the other hand, we assume the transmission charactestics remain consistent throghout the message, so you have to analyze the message as a whole to make decoding right. Consistency means that if in the beginning of a message '11111' is a dot and '111111' is a dash, then the same is true everywhere in that message. Moreover, it also means '00000' is definitely a short (in-character) pause, and '000000' is a long (between-characters) pause.
+
+That said, your task is to implement two functions:
+
+1. Function `decodeBitsAdvanced(bits)`, that should find an estimate for the transmission rate of the message, take care about slight speed variations that may occur in the message, correctly decode the message to dots `.`, dashes `-` and spaces (one between characters, three between words) and return those as a string. Note that some extra `0`'s may naturally occur at the beginning and the end of a message, make sure to ignore them. If message is empty or only contains `0`'s, return empty string. Also if you have trouble discerning if the particular sequence of `1`'s is a dot or a dash, assume it's a dot. If stuck, check [this](https://en.wikipedia.org/wiki/K-means_clustering) for ideas.
+
+2. Function `decodeMorse(morseCode)`, that would take the output of the previous function and return a human-readable string. If the input is empty string or only contains spaces, return empty string.
+
+**NOTE:** For coding purposes you have to use ASCII characters `.` and `-`, not Unicode characters.
+
+The Morse code table is preloaded for you as `MORSE_CODE` dictionary, feel free to use it. For C, the function `morse_code` acts like the dictionary. For C++ and Scala, a map is used. For C#, it's called `Preloaded.MORSE_CODE`. For Racket, a hash called MORSE-CODE is used.
+
+    (hash-ref MORSE-CODE "".-.") ; returns "C"
+    
+
+    const char *morse_code (const char *dotsdashes);
+    
+
+    extern map <string, string> morse_code;
+    
+
+    val MorseCodes.MORSE_CODE: Map[string, string];
+    
+
+Of course, not all messages may be fully automatically decoded. But you may be sure that all the test strings would be valid to the point that they could be reliably decoded as described above, so you may skip checking for errors and exceptions, just do your best in figuring out what the message is!
+
+Good luck!
+
+* * *
+
+Algorithms
+
+Decoding
+
+* * *
+
+  
+
+[
+
+powered by![](/assets/logos/qualified-black-b052752a4beaf94810c9d982f495680e2a9eb207824764ef98240ccef15cfbb1.svg)![](/assets/logos/qualified-white-7cba1bde874154ee4f39d50aebd5b7e435f5b21af9884b236a60d9015039e7f0.svg)
+
+](https://www.qualified.io?utm_source=codewars&utm_medium=web)
---
+++ src/csharp/CodingKata.Exercise/CodeWars/MorseCodeDecoderRealLife.cs
@@ -0,0 +1,365 @@
+﻿using CodingKata.Exercise.CodeWars.Utils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodingKata.Exercise.CodeWars.MorseCodeDecoderRealLife
+{
+    public interface IMorseCodeDecoderRealLife
+    {
+        string DecodeBitsAdvanced(string bits);
+        string DecodeMorse(string morseCode);
+    }
+    public class Kata : IMorseCodeDecoderRealLife
+    {
+        const string ShortPause = "";
+        const string LetterPause = " ";
+        const string WordPause = "   ";
+        const string Dot = ".";
+        const string Dash = "-";
+        const char Zero = '0';
+        const char One = '1'; 
+
+        public string DecodeBitsAdvanced(string bits)
+        {
+            StringBuilder decodedMorseFromBits = new StringBuilder();
+
+            // Trim 0 at start and end bits chain
+            string inputBits = bits.Trim(Zero);
+
+            if (inputBits.Length == 0)
+            {
+                return string.Empty;
+            }
+
+            if (inputBits.IndexOf(Zero) == -1)
+            {
+                return Dot;
+            }
+
+            // Convert to number list: 1110011111 => 3[1],2[0],5[1]
+            CustomList numberProjecion = ToWeightableProjection(inputBits);
+
+            // Clustering
+            var clusteredLabels = new KMeansClustering(numberProjecion.Distinct());
+
+            // Decode
+            foreach (var currentValue in numberProjecion)
+            {
+                string morse = clusteredLabels.GetLabel(currentValue); ;
+                decodedMorseFromBits.Append(morse);
+            }
+
+
+            return decodedMorseFromBits.ToString();
+        }
+
+        
+        public string DecodeMorse(string morseCode)
+        {
+            if (string.IsNullOrEmpty(morseCode))
+                return string.Empty;
+            string[] morseCodeWords = morseCode.Trim().Split("   ");
+            Dictionary<string, string> specials = new Dictionary<string, string>()
+            {
+                {"...−−−...", "SOS"}
+            };
+
+            string sentence = string.Empty;
+            for (int i = 0; i < morseCodeWords.Length; i++)
+            {
+                var textWord = string.Empty;
+                foreach (var morseLetter in morseCodeWords[i].Split(' '))
+                {
+                    string letter = Preloaded.MORSE_CODE[morseLetter];
+                    if (letter != string.Empty || specials.TryGetValue(morseLetter, out letter))
+                    {
+                        textWord += letter;
+                    }
+                }
+
+                if (textWord != string.Empty)
+                {
+                    sentence += textWord + " ";
+                }
+            }
+
+            return sentence.Trim();
+        }
+
+        /// <summary>
+        /// Convert bit elements to number. e.g. 110110100111 -> 2,-1,2,-2,3
+        /// </summary>
+        /// <param name="inputBits"></param>
+        /// <returns></returns>
+        private static CustomList ToWeightableProjection(string inputBits)
+        {
+            CustomList numberProjecion = new CustomList();
+            char state = '1';
+            int number = 0;
+
+            for (int i = 0; i < inputBits.Length; i++)
+            {
+                int coutingValue = 1;
+                if (state == inputBits[i])
+                {
+                    number += coutingValue;
+                }
+                else
+                {
+                    numberProjecion.Add(new MorseBits(state, number));
+                    // reset
+                    state = inputBits[i];
+                    number = coutingValue;
+                }
+
+                if (i == inputBits.Length - 1)
+                {
+                    numberProjecion.Add(new MorseBits(state, number));
+                }
+            }
+
+            return numberProjecion;
+        }
+
+        interface IWeightable
+        {
+            int Weight { get; }
+        }
+
+        struct MorseBits : IWeightable, IEquatable<MorseBits>
+        {
+            public int Weight { get; }
+            public int Bit { get; }
+
+            public char State { get; }
+
+            public MorseBits(char state, int weight)
+            {
+                this.State = state;
+                this.Bit = Convert.ToInt32(state.ToString());
+                this.Weight = weight;
+            }
+
+            public bool Equals(MorseBits other)
+            {
+                return this.Weight == other.Weight && this.Bit == other.Bit;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (obj is MorseBits)
+                {
+                    return Equals((MorseBits)obj);
+                }
+
+                return false;
+            }
+
+            public override int GetHashCode()
+            {
+                return this.State.GetHashCode() + this.Weight.GetHashCode();
+            }
+
+            public override string ToString()
+            {
+                return $"{this.State}|{this.Weight}";
+            }
+        }
+
+        class CustomList : ICollection<MorseBits>
+        {
+            private readonly List<MorseBits> list = new List<MorseBits>();
+
+
+            #region ICollection
+            public int Count => list.Count;
+
+            public bool IsReadOnly => true;
+
+            public void Add(MorseBits item)
+            {
+                list.Add(item);
+            }
+
+            public void Clear()
+            {
+                list.Clear();
+            }
+
+            public bool Contains(MorseBits item)
+            {
+                return list.Contains(item);
+            }
+
+            public void CopyTo(MorseBits[] array, int arrayIndex)
+            {
+                list.CopyTo(array, arrayIndex);
+            }
+
+            public IEnumerator<MorseBits> GetEnumerator()
+            {
+                return list.GetEnumerator();
+            }
+
+            public bool Remove(MorseBits item)
+            {
+                return list.Remove(item);
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return list.GetEnumerator();
+            }
+            #endregion
+
+            public override string ToString()
+            {
+                return list.Select(m => m.ToString()).Aggregate((m,n) => $"{m}-{n}" );
+            }
+        }
+
+        
+        class KMeansClustering
+        {
+            private readonly IEnumerable<MorseBits> samples;
+            private Dictionary<MorseBits, string> labels = new Dictionary<MorseBits, string>(); 
+            private MorseBitsCentroid[] Centroids { get; set; }
+
+            public KMeansClustering(IEnumerable<MorseBits> samples)
+            {
+                this.samples = samples;
+                DoWork();
+            }
+
+            private void DoWork()
+            {
+                var min = samples.Select(x=> x.Weight).Min();
+                // Set centroids
+                Centroids = new MorseBitsCentroid[]
+                {
+                    new MorseBitsCentroid(new MorseBits(One, min), Dot),
+                    new MorseBitsCentroid(new MorseBits(One, min * 3), Dash),
+                    new MorseBitsCentroid(new MorseBits(Zero, min), ShortPause),
+                    new MorseBitsCentroid(new MorseBits(Zero, min * 3), LetterPause),
+                    new MorseBitsCentroid(new MorseBits(Zero, min * 7), WordPause)
+                };
+
+                Centroids[2].Dependence = Centroids[0];
+                Centroids[3].Dependence = Centroids[1];
+
+                do
+                {
+                    foreach (MorseBitsCentroid centroid in Centroids)
+                    {
+                        centroid.ClusterItems.Clear();
+                    }
+                    foreach (MorseBits sample in samples)
+                    {
+                        var centroids = Centroids.Where(c => c.Current.Bit == sample.Bit).ToList();
+                        MorseBitsCentroid nearest = centroids[0];
+                        double currentDistance = nearest.CalculateDistance(sample);
+                        for (int i = 1; i < centroids.Count; i++)
+                        {
+                            double distance = centroids[i].CalculateDistance(sample);
+                            if (distance < currentDistance)
+                            {
+                                currentDistance = distance;
+                                nearest = centroids[i];
+                            }
+                        }
+
+                        nearest.ClusterItems.Add(sample);
+                    }
+
+                    // Re-calculate centroids
+                    foreach (var centroid in Centroids)
+                    {
+                        // We don't change state of centroid
+                        centroid.SetValue(new MorseBits(centroid.Current.State, centroid.GetWeightMean()));
+                    }
+                }
+                while (Centroids.Any(centroid => !centroid.IsSameWithPrevious()));
+            }
+
+            public string GetLabel(MorseBits morseBits)
+            {
+                foreach (MorseBitsCentroid centroid in Centroids)
+                {
+                    if (centroid.Contains(morseBits))
+                    {
+                        return centroid.Label;
+                    }
+                }
+
+                throw new ArgumentException("Sample not found");
+            }
+        }
+
+        class MorseBitsCentroid
+        {
+            public MorseBits Current { get; private set; }
+            public string Label { get; }
+            public MorseBits Previous { get; private set; }
+            public List<MorseBits> ClusterItems { get; private set; } = new List<MorseBits>();
+            public MorseBitsCentroid Dependence { get; set; }
+            public MorseBitsCentroid(MorseBits morseBit, string label)
+            {
+                Current = morseBit;
+                Label = label;
+            }
+
+            public void SetValue(MorseBits newValue)
+            {
+                if (Dependence != null && newValue.Weight > Dependence.Current.Weight)
+                {
+                    newValue = new MorseBits(newValue.State, Dependence.Current.Weight);
+                }
+
+                Previous = Current;
+                Current = newValue;
+            }
+
+            public bool IsSameWithPrevious()
+            {
+                return Current.Equals(Previous);
+            }
+
+            public int GetWeightMean()
+            {
+                if (ClusterItems.Count == 0)
+                {
+                    return this.Current.Weight;
+                }
+
+                return (int)Math.Round(ClusterItems.Average(x => x.Weight), 0);
+            }
+
+            public double CalculateDistance(MorseBits sample)
+            {
+                return Math.Sqrt(Math.Pow(Current.Bit - sample.Bit, 2)  + Math.Pow(Current.Weight - sample.Weight, 2)); 
+            }
+
+            public bool Contains(MorseBits sample)
+            {
+                return ClusterItems.Contains(sample);
+            }
+        }
+
+    }
+
+    public static class Preloaded
+    {
+        public static readonly MorseCodeMap MORSE_CODE = new MorseCodeMap();
+
+        public class MorseCodeMap
+        {
+            public string this[string morseCode]
+            {
+                get { return MorseCode.Get(morseCode); }
+            }
+        }
+    }
+}
