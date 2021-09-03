cfe211a92855d601ce083aeed2253c7348715955
Le, Vu Hoan Thieu <levu74@gmail.com>
2021-09-03 18:38:12 +0700

    Add benchmark

---
+++ src/csharp/CodingKata.Exercise/CodeWars/Benchmarks/MorseCodeDecoderRealLifeBenchmark.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using CodingKata.Exercise.Benchmark;
+using CodingKata.Exercise.CodeWars.MorseCodeDecoderRealLife;
+using CodingKata.Exercise.CodeWars.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingKata.Exercise.CodeWars.Benchmarks
+{
+    public class MorseCodeDecoderRealLifeBenchmark : IMySolutionBenchmark
+    {
+        [Params(MorseCodeBitLibrary.THE_QUICK_BROWN_FOX_JUMPS_OVER_THE_LAZY_DOG_RL, MorseCodeBitLibrary.HEY_JUDE_1_3)]
+        public string Bits { get; set; }
+      
+        [Benchmark]
+        public void MySolution()
+        {
+            new Kata().DecodeBitsAdvanced(Bits);
+        }
+
+        //[Benchmark]
+        //public void BestVote()
+        //{
+        //    new KataBestVote().DecodeBits(Bits);
+        //}
+    }
+}
